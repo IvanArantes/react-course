d83f33cfae120db5feca6a3b155377109da359bf
Ivan Arantes <ilvarantes@outlook.com>
2019-11-22 16:19:38 -0300

    Switched to arrow functions and class properties

--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class NewSyntax {
+  name = 'Ivan';
+}
